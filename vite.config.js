21fb4727e9234accbe9550679180c198c2b8852d
Narsinghobs <152276670+Narsinghobs@users.noreply.github.com>
2024-09-02 16:51:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "https://narsinghobs.github.io/react_learning/",
   plugins: [react()],
 })
